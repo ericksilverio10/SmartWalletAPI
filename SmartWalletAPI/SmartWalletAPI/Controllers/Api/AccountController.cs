3a46c1bd8cfade18f3ea8ded79c4ca5dad108978
Erick Silvério <ericksilverio@Air-de-Erick.mshome.net>
2023-08-25 23:03:01 -0300

    Criacao do projeto, estrutura de controllers e models

---
+++ SmartWalletAPI/SmartWalletAPI/Controllers/Api/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SmartWalletAPI.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [Route("get")]
+        public String Index()
+        {
+            return "Olá mundo!";
+        }
+    }
+}
+
---
+++ SmartWalletAPI/SmartWalletAPI/Models/Wallet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SmartWalletAPI.Models
+{
+	public class Wallet
+	{
+
+		public int id { get; set; }
+	}
+}
+
