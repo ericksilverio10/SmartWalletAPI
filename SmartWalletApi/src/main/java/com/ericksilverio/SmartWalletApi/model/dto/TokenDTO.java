02b9f95315b5c27237338dfd17579626f74b8f34
ericksilverio <erick258096@gmail.com>
2024-01-24 15:53:46 -0300

    remove

--- SmartWalletApi/src/main/java/com/ericksilverio/SmartWalletApi/model/dto/TokenDTO.java
+++
@@ -1,9 +0,0 @@
-package com.ericksilverio.SmartWalletApi.model.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-
-public record TokenDTO(String token) {
-}
